eba5c1962348e676227626a15ac599dcfb91fb6a
njaird-gif <njaird@gmail.com>
2026-09-01 16:47:57 -0500

    Update Item1.sql

--- Week3/Item1.sql
+++ Week3/Item1.sql
@@ -1,1 +1,36 @@
+-- WEEK 3 ITEM 1 --
+
+
+
+/* 
+a. Chef – show me the salary of each chef listed in the table. Ensure this salary is displayed
+correctly as the Irish pound. Do NOT hardcode the euro dollar sign.
+*/
+
+
+
+/*
+b. Show me the kitchen details by kitchen.
+*/
+
+
+
+/*
+c. Menu price – show me the unique menu items along with the price of those dishes.
+*/
+
+
+
+/*
+d. Show me the distribution of how orders are placed (in-person, online, phone). Show
+this as a sum of each, with an overall sum for all options.
+*/
+
+
+
+/*
+e. Show me the reservations and how many patrons receive their favorite table and those
+that don’t.
+*/
+
 
