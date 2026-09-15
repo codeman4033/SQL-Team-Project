d294b753ba4fd9d80a5d9842546aae09df88270d
Landon <klandonheez@gmail.com>
2026-09-15 16:01:08 -0500

    Update and rename Item 1 to Item 1.sql

---
+++ Week 4/Item 1.sql
@@ -0,0 +1,62 @@
+-- SQL Programming Advanced: Final Project --
+-- By: Osman Abdirahman, Landon Heezen, Nathan Aird, Cody Ferguson --
+
+-- WEEK 4 DELIVERABLES --
+
+USE Restaurant
+
+-- ITEM 1 --
+
+/* 
+a. Return the chef(s) that have a preferred vendor and what item(s) these chefs prefer
+from these vendors. Also include the price for these items.
+*/
+
+/*
+b. Return the sum of the ingredients by recipe. Also included with this will be the recipe
+items.
+*/
+
+/*
+c. Show me the ingredients used in each recipe and the prices listed for those dishes that
+use the ingredients.
+*/
+
+/*
+d. Show me the kitchen details. These details can either be all records or by individual
+chef.
+*/
+
+/*
+e. Return information on the customers that dine at the restaurant. This stored procedure
+should be able to run and return individual customer information
+*/
+
+-- ITEM 2 --
+/* Develop the following indexes for the tables listed below. Foe each table, select what you
+think is the appropriate field. Document why you made this selection. */
+
+-- a.
+
+
+
+-- b.
+
+
+
+-- c.
+
+
+
+-- d. 
+
+
+
+-- e.
+
+
+-- ITEM 3 --
+/* For two (2) tables above (your choice), develop stored procedures that will allow you to
+perform CRUD (create/insert/update/delete) operations against those tables. This means that
+each table will have one stored procedure for each of the following tasks: */
+
